358fda82eab194d5de9b734cd0b3ee29fc66eb26
Sophie Leung <104914674+LeungSophie@users.noreply.github.com>
2026-09-24 21:37:30 +0930

    create question data file

---
+++ question.js
@@ -0,0 +1,40 @@
+export const questions = [
+
+  {
+    id: 1,
+
+    chinese:
+      "月兔應該選擇哪一條路？",
+
+    english:
+      "Which path should the Moon Rabbit choose?",
+
+    options: {
+      A: {
+        chinese: "左邊的小路",
+        english: "Left path"
+      },
+
+      B: {
+        chinese: "石橋",
+        english: "Stone bridge"
+      },
+
+      C: {
+        chinese: "竹林",
+        english: "Bamboo forest"
+      },
+
+      D: {
+        chinese: "月光之門",
+        english: "Moon Gate"
+      }
+    },
+
+    correctAnswer: "D",
+
+    image: null,
+    audio: null
+  }
+
+];
